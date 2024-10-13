66420248bf1444f1c9a660475254f44d4c676df0
Jmoss <jose.mosh@gmail.com>
2024-10-12 22:34:05 -0500

    Manejo de Validaciones

--- src/main/java/com/jmoss/restaurantbackend/controller/ClienteController.java
+++ src/main/java/com/jmoss/restaurantbackend/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.jmoss.restaurantbackend.dto.response.ClienteResponseDto;
 import com.jmoss.restaurantbackend.entity.Cliente;
 import com.jmoss.restaurantbackend.repository.ClienteRepository;
 import com.jmoss.restaurantbackend.service.IClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,7 +27,7 @@ public class ClienteController {
     };
 
     @PostMapping()
-    public ClienteResponseDto createClientes( @RequestBody ClienteRequestDto clienteRequestDto ) {
+    public ClienteResponseDto createClientes( @RequestBody @Valid ClienteRequestDto clienteRequestDto ) {
         return clienteService.createCliente(clienteRequestDto);
     };
 
--- src/main/java/com/jmoss/restaurantbackend/dto/request/ClienteRequestDto.java
+++ src/main/java/com/jmoss/restaurantbackend/dto/request/ClienteRequestDto.java
@@ -2,25 +2,42 @@ package com.jmoss.restaurantbackend.dto.request;
 
 import com.jmoss.restaurantbackend.dto.response.ClienteResponseDto;
 import com.jmoss.restaurantbackend.entity.Cliente;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 
 @Data
 @Builder
 public class ClienteRequestDto {
 
-
+    @NotNull(message = "No se permiten valores nulos")
+    @NotEmpty(message = "No se permiten valores nulos")
+    @Size(min = 3, message = "Debe tener una longitud mayor a 3 caracteres")
     private String nombre;
 
+    @NotNull(message = "No se permiten valores nulos")
+    @NotEmpty(message = "No se permiten valores nulos")
+    @Size(min = 5, max = 100)
     private String apellidos;
 
+    @Email
+    @NotNull
+    @NotEmpty
     private String correo;
 
+    @NotNull(message = "No se permiten valores nulos")
+    @NotEmpty(message = "No se permiten valores nulos")
     private String nit;
 
+    @NotNull(message = "No se permiten valores nulos")
+    @NotEmpty(message = "No se permiten valores nulos")
     private String razonSocial;
 
+    @NotNull(message = "No se permiten valores nulos")
+    @NotEmpty(message = "No se permiten valores nulos")
+    @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{6,20})", message = "El password debe tener minimo 8 caracteres, mayusculas minusculas y al menos un numero y un caracter especial.")
     private String password;
 
     public static Cliente buildtoEntity(ClienteRequestDto clienteRequestDto)
